9c5b2b557294d43e29dde60357482a9e216360d6
nekar5 <mrkareo@gmail.com>
2023-05-07 17:54:48 +0300

    added traceables

---
+++ src/main/java/com/kpi/computergraphics/lab3/model/traceabletransformable/PolygonMesh.java
@@ -0,0 +1,36 @@
+package com.kpi.computergraphics.lab3.model.traceabletransformable;
+
+import com.kpi.computergraphics.lab3.model.IntersectionInfo;
+import com.kpi.computergraphics.lab3.model.base.Matrix;
+import com.kpi.computergraphics.lab3.model.base.Ray;
+import com.kpi.computergraphics.lab3.model.object.Triangle;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PolygonMesh implements Traceable {
+
+    private final List<Triangle> triangles;
+
+    public PolygonMesh(List<Triangle> triangles) {
+        this.triangles = triangles;
+    }
+
+    @Override
+    public Optional<IntersectionInfo> findIntersection(Ray ray) {
+        double minLength = Double.MAX_VALUE;
+        Optional<IntersectionInfo> closestIntersection = Optional.empty();
+        for (int i = 0; i < triangles.size() - 1; i++) {
+            Optional<IntersectionInfo> intersection = triangles.get(i).findIntersection(ray);
+            if (intersection.isPresent() && intersection.get().length() < minLength) {
+                minLength = intersection.get().length();
+                closestIntersection = intersection;
+            }
+        }
+        return closestIntersection;
+    }
+
+    public void transform(Matrix transformMatrix) {
+        triangles.forEach(triangle -> triangle.transform(transformMatrix));
+    }
+}
---
+++ src/main/java/com/kpi/computergraphics/lab3/model/traceabletransformable/TraceableGroup.java
@@ -0,0 +1,41 @@
+package com.kpi.computergraphics.lab3.model.traceabletransformable;
+
+import com.kpi.computergraphics.lab3.model.IntersectionInfo;
+import com.kpi.computergraphics.lab3.model.base.Ray;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class TraceableGroup<T extends Traceable> implements Traceable {
+    public List<T> traceableObjects;
+
+    public TraceableGroup(List<T> traceableObjects) {
+        this.traceableObjects = traceableObjects;
+    }
+
+    public TraceableGroup() {
+        this(new ArrayList<T>());
+    }
+
+    public void add(T traceableObject) {
+        traceableObjects.add(traceableObject);
+    }
+
+    public Optional <IntersectionInfo> findIntersection(Ray ray) {
+        Optional <IntersectionInfo> closestHit = null;
+        for (T traceableObject : traceableObjects) {
+            Optional<IntersectionInfo> currentHit = traceableObject.findIntersection(ray);
+
+            if (currentHit == null) {
+                continue;
+            }
+
+            closestHit = closestHit != null ?
+                    (closestHit.get().length() < currentHit.get().length() ? closestHit: currentHit)
+                    : currentHit;
+        }
+
+        return closestHit;
+    }
+}
