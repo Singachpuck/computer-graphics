a3d6fb398b7caa856fc072fd8b8fe3a7b9adf1b4
Vladyslav-Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2023-06-05 13:05:19 +0300

    Remove redundant `image` package.

--- src/main/java/com/kpi/computergraphics/lab3/model/image/ImgWriter.java
+++
@@ -1,9 +0,0 @@
-package com.kpi.computergraphics.lab3.model.image;
-
-import java.io.InputStream;
-
-public interface ImgWriter {
-    String getFormat();
-
-    InputStream write(ImgBuffer imgBuffer);
-}
